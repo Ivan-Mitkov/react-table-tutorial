8ac268e96424e3dd4595f4f77cdc129970b9082a
Ivan Ivanov <ivan.ivanov@app-streams.com>
2023-09-18 12:50:29 +0300

    add table columns

---
+++ src/components/columns.js
@@ -0,0 +1,10 @@
+export const COLUMNS = [
+  { Header: 'Id' },
+  { Header: 'First Name', accessor: 'first_name' },
+  { Header: 'Last Name', accessor: 'last_name' },
+  { Header: 'Email', accessor: 'email' },
+  { Header: 'Date of Birth', accessor: 'date_of_birth' },
+  { Header: 'Age', accessor: 'age' },
+  { Header: 'Country', accessor: 'country' },
+  { Header: 'Phone Number', accessor: 'phone' },
+];
