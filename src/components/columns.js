8ef36aac3ae3652100c9e55fb198784d0314f3d2
Ivan Ivanov <ivan.ivanov@app-streams.com>
2023-09-18 15:35:12 +0300

    transform cell value using Cell property

--- src/components/columns.js
+++ src/components/columns.js
@@ -1,9 +1,18 @@
+import { format } from 'date-fns';
+
 export const COLUMNS = [
   { Header: 'Id', Footer: 'Id', accessor: 'id' },
   { Header: 'First Name', Footer: 'First Name', accessor: 'first_name' },
   { Header: 'Last Name', Footer: 'Last Name', accessor: 'last_name' },
   { Header: 'Email', Footer: 'Email', accessor: 'email' },
-  { Header: 'Date of Birth', Footer: 'Date of Birth', accessor: 'date_of_birth' },
+  {
+    Header: 'Date of Birth',
+    Footer: 'Date of Birth',
+    accessor: 'date_of_birth',
+    Cell: ({ value }) => {
+      return format(new Date(value), 'dd/MM/yyyy');
+    },
+  },
   { Header: 'Age', Footer: 'Age', accessor: 'age' },
   { Header: 'Country', Footer: 'Country', accessor: 'country' },
   { Header: 'Phone Number', Footer: 'Phone Number', accessor: 'phone' },
