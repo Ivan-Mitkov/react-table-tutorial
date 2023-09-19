7abb066027b1a121e4974d56c5a1cd6848e67998
Ivan Ivanov <ivan.ivanov@app-streams.com>
2023-09-19 11:38:06 +0300

    row selection

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import './App.css';
-import PaginationTable from './components/PaginationTable';
+import RowSelection from './components/RowSelection';
 
 function App() {
   return (
-    <div className="App">
-      <PaginationTable />
+    <div>
+      <RowSelection />
     </div>
   );
 }
